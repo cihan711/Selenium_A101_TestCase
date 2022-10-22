8d407f59b6c544c5e0236d6b7cf944430ccb62d4
Cihan <cihan@Cihan-MacBook-Pro.local>
2022-10-22 14:33:56 +0300

    again cucumber files update2

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -80,6 +80,7 @@ public class stepDefination extends base {
 	    
 	    
 	   //payment page and final case
+	    //driver is close automatically
 	    @When("user check payment page title")
 	    public void user_check_payment_page_title() throws InterruptedException {
 	    	ShoppingPage shp = new ShoppingPage(driver);
